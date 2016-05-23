6c69856c78feb1db891ff71d6c955cb2c2f95533
Justin Shayesteh <justinshayesteh@yahoo.com>
2016-05-23 17:32:42 -0400

    testing

---
+++ exercise-1.js
@@ -0,0 +1,40 @@
+/**
+ * For each of the following Array methods,
+
+ * 1. use them in an example
+ * 2. use console.log() to make the code run in the console!
+
+ **/
+ var arr = [3,2,5];
+ var n = 2;
+ console.log(arr)
+
+ Native Browser JavaScript
+
+[ 3, 2, 5 ]
+   
+// .sort()
+
+// .concat()
+
+// .indexOf()
+
+// .split()
+
+// .join()
+
+// .pop()
+
+// .push()
+
+// .slice()
+
+// .splice()
+
+// .shift()
+
+// .unshift()
+
+// .filter()
+
+// .map()
